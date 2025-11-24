1342d9403d83971520c9c7e74165ecb078647f6c
BigDsaur <Joona879@gmail.com>
2025-11-24 17:23:14 +0200

    render

--- Fullstackopen_3/puhelinluettelo_frontend/src/services/person.js
+++ Fullstackopen_3/puhelinluettelo_frontend/src/services/person.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = '/api/persons'
+const baseUrl = 'https://fullstackopen-3-5-backend.onrender.com/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)

f6e29cabe66ff76525baa3764cb220801c9a8d7c
BigDsaur <Joona879@gmail.com>
2025-11-26 01:56:35 +0200

    4.9: blogilistan testit, step2 id testi meni läpi

--- Fullstackopen_4/tests/blog_api.test.js
+++ Fullstackopen_4/tests/blog_api.test.js
@@ -37,6 +37,13 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(initialBlogs.length)
 })
 
+test('unique identifier property of blogs is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
+})
+
+
 afterAll(async () => {
   await mongoose.connection.close()
 })

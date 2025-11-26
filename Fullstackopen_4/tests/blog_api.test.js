b2f45e0763ba3c288ab2adb345e992730596ccd9
BigDsaur <Joona879@gmail.com>
2025-11-26 02:13:16 +0200

    4.10: blogilistan testit, step3 testit läpi

--- Fullstackopen_4/tests/blog_api.test.js
+++ Fullstackopen_4/tests/blog_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
@@ -41,6 +42,28 @@ test('unique identifier property of blogs is named id', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body[0].id).toBeDefined()
+})
+
+test('if likes property is missing, it defaults to 0', async () => {
+  const newBlog = {
+    title: 'Blog without likes field',
+    author: 'Tester',
+    url: 'http://nolikes.com'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  // Check the response
+  expect(response.body.likes).toBe(0)
+
+  // Optional: verify in database
+  const blogsAtEnd = await helper.blogsInDb()
+  const created = blogsAtEnd.find(b => b.title === 'Blog without likes field')
+  expect(created.likes).toBe(0)
 })
 
 
--- Fullstackopen_4/tests/test_helper.js
+++ Fullstackopen_4/tests/test_helper.js
@@ -2,20 +2,19 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-    title: "First blog",
-    author: "John Doe",
-    url: "http://example.com",
-    likes: 5
+    title: 'First test blog maurilta',
+    author: 'Tester One',
+    url: 'http://example1.com',
+    likes: 1,
   },
   {
-    title: "Second blog",
-    author: "Jane Doe",
-    url: "http://example2.com",
-    likes: 7
-  }
+    title: 'Second test blog matilta',
+    author: 'Tester Two',
+    url: 'http://example2.com',
+    likes: 2,
+  },
 ]
 
-// Returns all blogs in DB as plain JS objects
 const blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map(b => b.toJSON())
